ceeeb95e23820ed54181f3a177baf37c942adcfe
Abdulnaseer <abdulnasir.alkhatib@gmail.com>
2023-03-18 12:50:51 -0400

    adding splash view body

--- lib/features/Splash/presentation/views/splash_view.dart
+++ lib/features/Splash/presentation/views/splash_view.dart
@@ -1,12 +1,13 @@
+import 'package:book_store/features/Splash/presentation/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:book_store/features/Splash/presrntation/views/splash_view.dart';
+import 'package:book_store/features/Splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
